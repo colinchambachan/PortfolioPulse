26d979458f346d6ac4637fb3fbb00fb855de1f72
Colin <colin.chambachan@gmail.com>
2025-02-22 00:28:36 -0500

    feat: add ip-based rate limiting

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from decimal import Decimal
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request, HTTPException
+from fastapi.responses import JSONResponse
 import pytesseract
 from PIL import Image
 import io
@@ -9,17 +10,30 @@ import re
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import boto3
-import env
-
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 
 app = FastAPI()
 
+# Initialize the Limiter
+limiter = Limiter(key_func=get_remote_address)
+
+# Add the SlowAPI middleware
+app.add_exception_handler(RateLimitExceeded, lambda request, exc: JSONResponse(
+    status_code=429,
+    content={"message": f"Rate limit exceeded: {exc.detail}"}
+))
+
+app.add_middleware(SlowAPIMiddleware)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://portfoliopulse.xyz" ], 
+    allow_origins=["http://localhost:3000", "https://portfoliopulse.xyz"],
     allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 def get_aws_credentials():
@@ -34,41 +48,52 @@ def get_ddb_connection():
     return dynamodb
 
 @app.get("/")
+@limiter.limit("5/minute")
 def read_root(user: Union[str, None] = None):
     return {"Hello": user}
 
-
 @app.post("/user")
+@limiter.limit("2/minute")
 async def create_user(obj: dict):
-    print(obj["email"], obj["portfolio"])
-    ddb = get_ddb_connection()
-    TABLE_NAME = os.getenv("TABLE_NAME")
-    table = ddb.Table(TABLE_NAME)
-    for symbol, quantity in obj["portfolio"].items():
-        table.put_item(Item={
-            "user": obj["email"],
-            "quantity": Decimal(str(quantity)),
-            "symbol": str(symbol)
-        })
-    return {"message": "User created successfully!"}
+    try:
+        ddb = get_ddb_connection()
+        TABLE_NAME = os.getenv("TABLE_NAME")
+        table = ddb.Table(TABLE_NAME)
+        for symbol, quantity in obj["portfolio"].items():
+            try:
+                table.put_item(
+                    Item={
+                        "user": obj["email"],
+                        "quantity": Decimal(str(quantity)),
+                        "symbol": str(symbol)
+                    },
+                    ConditionExpression="attribute_not_exists(user) AND attribute_not_exists(quantity) AND attribute_not_exists(symbol)"
+                )
+            except ddb.meta.client.exceptions.ConditionalCheckFailedException:
+                continue
+        return {"message": "User created successfully!"}
+    except Exception as error:
+        return {"error": str(error)}    
 
 @app.delete("/user")
+@limiter.limit("2/minute")
 async def delete_user(email: str):
-    ddb = get_ddb_connection()
-    TABLE_NAME = os.getenv("TABLE_NAME")
-    table = ddb.Table(TABLE_NAME)
-    scan = table.scan()
-    with table.batch_writer() as batch:
-        for each in scan['Items']:
-            if each['user'] == email:
-                batch.delete_item(Key={
-                    "user": email,
-                    "quantity": each['quantity']
-                })
-    response = {"message": f"All items with user {email} have been deleted."}
-    return response
-
-
+    try:
+        ddb = get_ddb_connection()
+        TABLE_NAME = os.getenv("TABLE_NAME")
+        table = ddb.Table(TABLE_NAME)
+        scan = table.scan()
+        with table.batch_writer() as batch:
+            for each in scan['Items']:
+                if each['user'] == email:
+                    batch.delete_item(Key={
+                        "user": email,
+                        "quantity": each['quantity']
+                    })
+        response = {"message": f"All items with user {email} have been deleted."}
+        return response
+    except Exception as error:
+        return {"error": str(error)}
 
 def extract_stock_data(text: str):
     stock_data = {}
