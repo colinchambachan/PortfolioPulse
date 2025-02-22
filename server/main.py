378f8b90ef195f4c9f94678603d567bdd3c2c293
Colin <colin.chambachan@gmail.com>
2025-02-21 21:21:17 -0500

    feat: update create and delete user route

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from decimal import Decimal
 from fastapi import FastAPI, File, UploadFile
 import pytesseract
 from PIL import Image
@@ -6,7 +7,9 @@ import io
 from pdf2image import convert_from_bytes
 import re
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
+import boto3
+import env
 
 
 app = FastAPI()
@@ -19,9 +22,51 @@ app.add_middleware(
     allow_headers=["*"], 
 )
 
+def get_aws_credentials():
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    region = os.getenv("AWS_REGION")
+    return aws_access_key_id, aws_secret_access_key, region
+
+def get_ddb_connection():
+    aws_access_key_id, aws_secret_access_key, region = get_aws_credentials()
+    dynamodb = boto3.resource('dynamodb', region_name=region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+    return dynamodb
+
 @app.get("/")
 def read_root(user: Union[str, None] = None):
     return {"Hello": user}
+
+
+@app.post("/user")
+async def create_user(obj: dict):
+    print(obj["email"], obj["portfolio"])
+    ddb = get_ddb_connection()
+    TABLE_NAME = os.getenv("TABLE_NAME")
+    table = ddb.Table(TABLE_NAME)
+    for symbol, quantity in obj["portfolio"].items():
+        table.put_item(Item={
+            "user": obj["email"],
+            "quantity": Decimal(str(quantity)),
+            "symbol": str(symbol)
+        })
+    return {"message": "User created successfully!"}
+
+@app.delete("/user")
+async def delete_user(email: str):
+    ddb = get_ddb_connection()
+    TABLE_NAME = os.getenv("TABLE_NAME")
+    table = ddb.Table(TABLE_NAME)
+    scan = table.scan()
+    with table.batch_writer() as batch:
+        for each in scan['Items']:
+            if each['user'] == email:
+                batch.delete_item(Key={
+                    "user": email,
+                    "quantity": each['quantity']
+                })
+    response = {"message": f"All items with user {email} have been deleted."}
+    return response
 
 
 
