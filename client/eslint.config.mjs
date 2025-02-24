418a73b87258c58b6e3cb7ea9dd8848f79311fad
colin.chamabchan@gmail.com <colin.chambachan@gmail.com>
2025-02-23 22:12:26 -0500

    chore: update config

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable all rules
+      "no-unused-vars": "off",
+      "no-console": "off",
+      // Add other rules here as needed
+    },
+  },
 ];
 
 export default eslintConfig;
